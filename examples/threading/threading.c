e0cc76871b7368411f931679da0883e9f9b85ad9
swagathu <unnithanswagath@gmail.com>
2023-01-09 09:57:00 +0000

    Assignment 4 submission 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,21 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int ret = 0, ret1 = 0;
+    struct thread_data * params = (struct thread_data *) thread_param;
+    
+    usleep(params->wait_to_obtain_ms *1000);
+    ret += pthread_mutex_lock(params->mutex);
+    usleep(params->wait_to_release_ms *1000);
+    ret += pthread_mutex_unlock(params->mutex);
+    if (ret == 0 && ret1 == 0)
+    {
+        params->thread_complete_success = true;
+    }
+    else
+    {
+        params->thread_complete_success = false;
+    }
     return thread_param;
 }
 
@@ -28,6 +43,20 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
+    int ret = 0;
+    struct thread_data * thread_param;
+    thread_param = malloc(sizeof(struct thread_data));
 
+    thread_param->mutex = mutex;
+    thread_param->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_param->wait_to_release_ms = wait_to_release_ms;
+    
+    ret = pthread_create(thread, NULL, &threadfunc, (void *)thread_param);
+    if (ret != 0)
+    {
+        printf("Error thread creation: %s\n",strerror(errno));
+        return false;
+    }
+
+    return true;
+}
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <pthread.h>
-
+#include <string.h>
+#include <errno.h>
 /**
  * This structure should be dynamically allocated and passed as
  * an argument to your thread using pthread_create.
@@ -14,7 +15,9 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
-
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
+    pthread_mutex_t * mutex;
     /**
      * Set to true if the thread completed with success, false
      * if an error occurred.
