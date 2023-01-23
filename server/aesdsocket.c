69381a2db54ffc96dabf028e3a4d092a200bc77d
swagathu <unnithanswagath@gmail.com>
2023-01-23 10:16:33 +0000

    assignment 5

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -13,6 +13,7 @@
 #define MAX_CONN_Q  1
 #define ADD_SPACE   1024 //bytes
 #define BUF_MAX_LEN 1024
+//#define LOGFILE     "/var/tmp/aesdsocketdata"
 #define LOGFILE     "./socketdata_log"
 
 int switchoff = 0;
@@ -23,20 +24,16 @@ void sig_handle(int signal)
     return;
 }
 
-void sig_handle_alrm(int signal)
-{
-    printf("alarm\n");
-    return;
-}
-
 struct mem_resp {
     char * data;
     long int len;
     long int total;
+    long int saved;//total len saved already.
 };
 
 typedef struct mem_resp resp;
 
+//realloc and save any amount of data to data buffer:
 long int store_to_resp(resp * resp_struct, char * buf, int buflen)
 {
     if ( resp_struct->len + buflen < resp_struct->total )
@@ -57,14 +54,15 @@ long int store_to_resp(resp * resp_struct, char * buf, int buflen)
         else {
             tmp_sz = resp_struct->total + ADD_SPACE;
         }
-        tmp = realloc(resp_struct->data, resp_struct->len + ADD_SPACE);
+        tmp = realloc(resp_struct->data, tmp_sz);
         if (tmp == NULL)
         {
-            printf("realloc fail: %s", strerror(errno));
+            syslog(LOG_ERR, "realloc fail: %s", strerror(errno));
             return 0;
         }
         resp_struct->data  = tmp;
         resp_struct->total = tmp_sz;
+        memset(resp_struct->data + resp_struct->len, '\0', resp_struct->total - resp_struct->len);
         memcpy( resp_struct->data + resp_struct->len , buf, buflen );
         resp_struct->len   = resp_struct->len + buflen;
         return resp_struct->len;
@@ -81,15 +79,16 @@ int store_to_file(resp * resp_struct)
     fp = fopen(LOGFILE, "a");
     if (fp == NULL)
     {
-        printf("Error in file open: %s", strerror(errno));
+        syslog(LOG_ERR, "Error in opening file %s : %s", LOGFILE, strerror(errno));
         return -1;
     }
-    int ret = fwrite(resp_struct->data, 1, resp_struct->len, fp);
-    if (ret < resp_struct->len)
+    int ret = fwrite(resp_struct->data + resp_struct->saved, 1, resp_struct->len - resp_struct->saved, fp);
+    if (ret < (resp_struct->len - resp_struct->saved))
     {
-        printf("Error in file write not fully written.\n");
+        syslog(LOG_ERR, "Error in writing file %s: %s", LOGFILE, strerror(errno));
         return -1;
     }
+    resp_struct->saved = resp_struct->len;
     fclose(fp);
     return 0;
 }
@@ -106,49 +105,39 @@ void free_resp(resp * resp_struct)
     resp_struct->len   = 0;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     signal (SIGINT, sig_handle);
     signal (SIGTERM, sig_handle);
     //setting up the signals:
     struct sigaction act_int;
     act_int.sa_handler = sig_handle;
+    sigemptyset(&act_int.sa_mask);
+    act_int.sa_flags = 0;
     if (sigaction( SIGINT, &act_int , NULL) == -1)
     {
-        printf("Error in sigint handle prep: %s\n", strerror(errno));
         syslog(LOG_ERR, "Error in sigint handle prep: %s", strerror(errno));
         return -1;
     }
-
-    if (sigaction( SIGTERM, &act_int , NULL) == -1)
+    struct sigaction act_term;
+    act_term.sa_handler = sig_handle;
+    sigemptyset(&act_term.sa_mask);
+    act_term.sa_flags = 0;
+    if (sigaction( SIGTERM, &act_term , NULL) == -1)
     {
-        printf("Error in sigterm handle prep: %s\n", strerror(errno));
         syslog(LOG_ERR, "Error in sigterm handle prep: %s", strerror(errno));
         return -2;
     }
-
-    struct sigaction act_alrm;
-    act_alrm.sa_handler = sig_handle_alrm;
-    sigemptyset(&act_alrm.sa_mask);
-    act_alrm.sa_flags = 0;
-    if (sigaction( SIGALRM, &act_alrm , NULL) == -1) 
-    {   
-        printf("Error in sigalrm handle prep: %s\n", strerror(errno));
-        syslog(LOG_ERR, "Error in sigalrm handle prep: %s", strerror(errno));
-        return -3; 
-    }
-
 
     int sockfd;
     int domain = PF_INET;
     int type = SOCK_STREAM;
     int protocol = 0;
     int ret = 0;
-    openlog(NULL, LOG_CONS || LOG_NDELAY || LOG_PID || LOG_PERROR, LOG_USER);
+    openlog(NULL, LOG_CONS | LOG_NDELAY | LOG_PID | LOG_PERROR, LOG_USER);
     sockfd = socket(domain, type, protocol);
     if (sockfd == -1)
     {
-        printf("socket creation error: %s\n", strerror(errno));
         syslog(LOG_ERR, "socket creation error: %s", strerror(errno));
         return 1;
     }
@@ -166,28 +155,58 @@ int main()
     {
         if (ret != EAI_SYSTEM)
         {
-            printf("getaddrinfo() fail: %s\n", gai_strerror(ret));
             syslog(LOG_ERR, "getaddrinfo() fail: %s", gai_strerror(ret));
         }
         else
         {
-            printf("getaddrinfo() fail: %s\n", strerror(errno));
             syslog(LOG_ERR, "getaddrinfo() fail: %s", strerror(errno));
         }
+        freeaddrinfo(res);
         return 1;
+    }
+
+    //reuse address and avoid time wait when trying to close socket:
+    int optval = 1; 
+    ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR , &optval, sizeof(int));
+    if (ret == -1)
+    {
+        syslog(LOG_ERR, "setopt fail: %s", strerror(errno));
     }
 
     ret = bind (sockfd, res->ai_addr, sizeof(struct sockaddr));
     if(ret == -1)
     {
-        printf("bind adress failed: %s\n", strerror(errno));
         syslog(LOG_ERR, "bind adress failed: %s", strerror(errno));
+        freeaddrinfo(res);
         return 2;
     }
+
+    //check to seee if -d argument present (overdone):
+    if (argc > 1)
+    {
+        for (int i ; i<argc; i++)
+        {
+            if (strcmp("-d", argv[i]) == 0)
+            {
+                switch ((ret = fork()))
+                {
+                    case -1:
+                        syslog(LOG_ERR, "Error on fork: %s", strerror(errno));
+                        break;
+                    case 0:
+                        syslog(LOG_DEBUG, "Started as a deamon");
+                        break;
+                    default:
+                        exit(EXIT_SUCCESS);
+                        break;
+                }
+            }
+        }
+    }
+
     ret = listen(sockfd, MAX_CONN_Q);
     if (ret == -1)
     {
-        printf("listen error: %s\n", strerror(errno));
         syslog(LOG_ERR, "listen error on PORT: %s: %s", PORT, strerror(errno));
         return 3;
     }
@@ -200,33 +219,38 @@ int main()
     int recvret = 0;
     char buf[BUF_MAX_LEN];
     int buflen = 0;
+
     //steup resp structure:
     resp data;
     data.data = NULL;
     data.len = 0;
     data.total = 0;
+    data.saved = 0;
+    
+    //main loop:
     while (1)
     {
+        //if signal is caught free everything:
         if (switchoff == 1)
         {
             freeaddrinfo(res);
             free_resp(&data);
             close(conn);
+            close(sockfd);
+            remove(LOGFILE);
+            syslog(LOG_DEBUG, "Caught signal, exiting");
             exit(1);
         }
-        alarm(5);//block accept for 5sec then interupp using sigalrm.
+        memset (buf, '\0', BUF_MAX_LEN);
         conn = accept(sockfd, &addr, &addrlen);
-        printf("conn: %d\n", conn);
         if (conn == -1)
         {
-            printf("accept error: %s\n", strerror(errno));
             syslog(LOG_ERR, "accept error: %s", strerror(errno));
             continue;
         } else {
-            printf("Accepted connection from %d\n", ntohs(add_disp->sin_port));
-            syslog(LOG_DEBUG, "Connection accepted from: %s", addr.sa_data);
+            syslog(LOG_DEBUG, "Connection accepted from: %d", ntohs(add_disp->sin_port));
         }
-
+        //read till there is nothing to read:
         while(1)
         {
             buflen = recv( conn, buf, BUF_MAX_LEN, MSG_DONTWAIT);
@@ -237,20 +261,16 @@ int main()
                     ret = store_to_file(&data);
                     if (ret != 0)
                     { 
-                        printf("Cannot write to file %s\n", LOGFILE);
                         syslog(LOG_ERR, "Cannot write to file: %s", LOGFILE);
                     }
-                    //send back the data:
-                    ret = send( conn, data.data, data.len, MSG_CONFIRM);
+                    ret = send( conn, data.data, data.len, 0);
                     if (ret < data.len)
                     {
-                        printf("Cannot send data: %s\n", strerror(errno));
                         syslog(LOG_ERR, "Cannot send data: %s", strerror(errno));
                     }
                 }
                 break;
              }
-             printf("%s", buf);
              ret = store_to_resp( &data, buf, buflen);
              if (ret == 0)
              {
@@ -259,11 +279,10 @@ int main()
              }
         }
 
-        free_resp(&data);
         close(conn);
-        printf("Closed connection from %d\n",ntohs(add_disp->sin_port));
-        syslog(LOG_DEBUG, "Closed connection from %s", addr.sa_data);
+        syslog(LOG_DEBUG, "Closed connection from %d", ntohs(add_disp->sin_port));
     }
 
     freeaddrinfo(res);
+    return 0;
 }
